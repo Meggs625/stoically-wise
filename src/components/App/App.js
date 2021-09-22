37b4c7aa1cf036ace14408c0ae137d96c70b14a6
Megan McBride <megan.mcbride@colorado.edu>
2021-09-22 14:46:35 -0600

    Setting up changes to try to get the quote page to display

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,35 +8,13 @@ import './App.css';
 const App = () => {
 
   const [chosenTheme, setChosenTheme] = useState('');
-  // const [quotes, setQuotes] = useState([]);
-  // const [pics, setPics] = useState([]);
+
 
   const updateTheme = (inputTheme) => {
     setChosenTheme(inputTheme);
   }
 
-  // const fetchData = () => {
-  //   return fetch('https://stoic-server.herokuapp.com/random', {
-  //   })
-  //     .then(res => res.json())
-  //     .then(data => setQuotes(data))
-  // }
 
-  // const fetchPhotos = () => {
-  //   return fetch(`https://pixabay.com/api/?key=23483948-a9995475fd38e7480dc10e8df&q=${chosenTheme}&image_type=photo`)
-  //     .then(res => res.ok ? res.json() : console.log(res))
-  //     .then(data => setPics(data.hits))
-  //     .catch(err => console.log(err))
-  // }
-  // const randomPic = () => {
-  //   const randomIndex =  Math.floor(Math.random() * pics.length)
-  //   return pics[randomIndex].largeImageURL
-  // }
-
-  // useEffect(() => {
-  //   fetchData()
-  //   fetchPhotos()
-  // })
 
   return (
     <main>
--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Quote.css';
 
 const Quote = ({ theme }) => {
+
+  const [quote, setQuote] = useState([]);
+  const [pics, setPics] = useState([]);
+
+  const fetchData = () => {
+    return fetch('https://stoic-server.herokuapp.com/random', {
+    })
+      .then(res => res.json())
+      .then(data => setQuote(data))
+  }
+
+  const fetchPhotos = () => {
+    return fetch(`https://pixabay.com/api/?key=23483948-a9995475fd38e7480dc10e8df&q=${theme}&image_type=photo`)
+      .then(res => res.ok ? res.json() : console.log(res))
+      .then(data => setPics(data.hits))
+      .catch(err => console.log(err))
+  }
+  const randomPic = () => {
+    const randomIndex =  Math.floor(Math.random() * pics.length)
+    
+    return pics[randomIndex].largeImageURL
+  }
+
+  useEffect(() => {
+    fetchData()
+    // fetchPhotos()
+  }, [])
+
+  useEffect(() => {
+    fetchPhotos()
+  }, [])
+
+  // const displayInfo = () => {
+  //   return (
+      
+  //   )
+  // }
+
   return (
-    <h1>Made it</h1>
-  )
+   
+    <section className='main-display' style={{backgroundImage: `url('${randomPic()}')`, backgroundColor: 'rgba(0,0,0,0.5)', opacity: '.75', repeat: 'none'}}>
+      <section className='card'>
+        <h2 className='quote'>{quote.body}</h2>
+        <p className='author'>{quote.author}</p>
+      </section>    
+    </section>  
+    )
 
 }
 
 export default Quote;
+
+
