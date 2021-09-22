c64ec28d73b15e52017a8a8ff112d4da9e2893ba
Megan McBride <megan.mcbride@colorado.edu>
2021-09-22 09:53:11 -0600

    Add API calls but they need to be moved to Quote component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import ThemeForm from '../ThemeForm/ThemeForm';
 import Navbar from '../Navbar/Navbar';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [chosenTheme, setChosenTheme] = useState('');
+  const [quotes, setQuotes] = useState([]);
+  const [pics, setPics] = useState([]);
+
+  const updateTheme = (inputTheme) => {
+    setChosenTheme(inputTheme);
+  }
+
+  const fetchData = () => {
+    return fetch('https://stoic-server.herokuapp.com/random', {
+    })
+      .then(res => res.json())
+      .then(data => setQuotes(data))
+  }
+
+  const fetchPhotos = () => {
+    return fetch(`https://pixabay.com/api/?key=23483948-a9995475fd38e7480dc10e8df&q=${chosenTheme}&image_type=photo`)
+      .then(res => res.ok ? res.json() : console.log(res))
+      .then(data => setPics(data.hits))
+      .catch(err => console.log(err))
+  }
+  const randomPic = () => {
+    const randomIndex =  Math.floor(Math.random() * pics.length)
+    return pics[randomIndex].largeImageURL
+  }
+
+  useEffect(() => {
+    fetchData()
+    fetchPhotos()
+  })
+
   return (
     <main>
       <Navbar />
       <Switch>
         <Route exact path='/' render={() => 
-          <div>
+          <div className='card'style={{backgroundImage: `url('${randomPic()}')`, width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.5)', opacity: '.75'/*add no repeat*/}}>
             <h1>Hello!</h1>
-            <ThemeForm /> 
+            <ThemeForm updateTheme={updateTheme}/> 
           </div>}
         />
 
