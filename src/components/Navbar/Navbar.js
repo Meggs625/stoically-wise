6a4a08bfc823c4fb293caa0705b5bf61c64a6d2b
Megan McBride <megan.mcbride@colorado.edu>
2021-09-22 10:39:48 -0600

    Style and make header

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Navbar.css';
+import owlLogo from '../../owl.png';
 
 const Navbar = () => {
   return (
-    <h1>Let me guide you....</h1>
+    <div className='brand'>
+      <img src={owlLogo} alt='owl logo' className='logo'/>
+      <h1 className='brand-name'>Daily Wisdom</h1>
+    </div>
   )
 }
 
