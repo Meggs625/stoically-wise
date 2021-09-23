69684c822ca6581cf0bbd1833ea6a7e544ff2525
Megan McBride <megan.mcbride@colorado.edu>
2021-09-23 09:40:28 -0600

    Conditionals for navigation links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
 import './Navbar.css';
 import owlLogo from '../../owl.png';
 
 const Navbar = () => {
+  const location = useLocation();
   return (
-    <div className='brand'>
-      <img src={owlLogo} alt='owl logo' className='logo'/>
-      <h1 className='brand-name'>Daily Wisdom</h1>
-    </div>
+    <section className='nav-bar'>    
+      <div className='brand'>
+        <img src={owlLogo} alt='owl logo' className='logo'/>
+        <h1 className='brand-name'>Daily Wisdom</h1>
+      </div>
+      {location.pathname === '/quote' && 
+        <Link to='/' className='new-theme-link'>Select New Theme</Link>}
+    </section>
   )
 }
 
