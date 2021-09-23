d2bf4ac3ab2a44929cde06e17b6d5d39ce461783
Megan McBride <megan.mcbride@colorado.edu>
2021-09-22 19:28:47 -0600

    Trying to fix quote display

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -3,7 +3,7 @@ import './Quote.css';
 
 const Quote = ({ theme }) => {
 
-  const [quote, setQuote] = useState([]);
+  const [quotes, setQuote] = useState([]);
   const [pics, setPics] = useState([]);
 
   const fetchData = () => {
@@ -14,39 +14,46 @@ const Quote = ({ theme }) => {
   }
 
   const fetchPhotos = () => {
-    return fetch(`https://pixabay.com/api/?key=23483948-a9995475fd38e7480dc10e8df&q=${theme}&image_type=photo`)
+    return fetch('https://pixabay.com/api/?key=23483948-a9995475fd38e7480dc10e8df&q=birds&image_type=photo')
       .then(res => res.ok ? res.json() : console.log(res))
       .then(data => setPics(data.hits))
       .catch(err => console.log(err))
   }
   const randomPic = () => {
     const randomIndex =  Math.floor(Math.random() * pics.length)
-    
+    console.log((randomIndex), 'index')
+    console.log(pics[1])
     return pics[randomIndex].largeImageURL
   }
 
   useEffect(() => {
     fetchData()
-    // fetchPhotos()
-  }, [])
-
-  useEffect(() => {
     fetchPhotos()
   }, [])
 
+
+  const displayInfo = () => {
+    return quotes.map(quote => (
+      <section className='card'style={{backgroundImage: `url('${randomPic()}')`, backgroundColor: 'rgba(0,0,0,0.5)', opacity: '.75'/*add no repeat*/}}>
+        <h2 className='quote'>{quote.body}</h2>
+        <p className='author'>{quote.author}</p>
+      </section>
+    ))
+  }
+
   // const displayInfo = () => {
   //   return (
-      
+  //     <section className='card' style={{backgroundImage: `url('${pics[0].largeImageURL}')`, backgroundColor: 'rgba(0,0,0,0.5)', opacity: '.75', repeat: 'none'}}>
+  //       <h2 className='quote'>{quote.body}</h2>
+  //       <p className='author'>{quote.author}</p>
+  //     </section>
   //   )
   // }
 
   return (
    
-    <section className='main-display' style={{backgroundImage: `url('${randomPic()}')`, backgroundColor: 'rgba(0,0,0,0.5)', opacity: '.75', repeat: 'none'}}>
-      <section className='card'>
-        <h2 className='quote'>{quote.body}</h2>
-        <p className='author'>{quote.author}</p>
-      </section>    
+    <section className='main-display' >
+      {displayInfo()}
     </section>  
     )
 
