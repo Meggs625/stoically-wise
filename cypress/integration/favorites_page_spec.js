2e3fd2a620b7588669cf7550cba592d264165b2c
Megan McBride <megan.mcbride@colorado.edu>
2021-09-26 15:49:08 -0600

    Add more favorites page testing

--- cypress/integration/favorites_page_spec.js
+++ cypress/integration/favorites_page_spec.js
@@ -15,4 +15,26 @@ describe('Favorites Page', () => {
   it('Should include the quote that was saved', () => {
     cy.get('.saved-quote').should('contain', 'The art of life')
   })
+
+  it('Should display the saved image', () => {
+    cy.get('.saved-img').should('have.attr', 'src')
+  })
+
+  it('Should display the author of the quote', () => {
+    cy.get('.saved-author').should('contain', 'Marcus Aurelius')
+  })
+
+  it('Should have a button to remove the quote from the saved quotes', () => [
+    cy.get('.delete-icon').should('be.visible')
+  ])
+
+  it('Should no longer display the card after clicking the delete button', () => {
+    cy.get('.delete-icon').click()
+      .get('.card').should(('not.exist'))
+  })
+
+  it('Should display a message if there are no favorites to display', () => {
+    cy.get('.delete-icon').click()
+      .get('.no-favorites').should('be.visible').should('contain', 'Nothing saved yet')
+  })
 })
