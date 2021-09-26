7b5bae40bb9b315af96e9dc17e3d32db983c5456
Megan McBride <megan.mcbride@colorado.edu>
2021-09-26 09:29:53 -0600

    Add props for the FavoriteCard to display

--- src/components/FavoriteCard/FavoriteCard.js
+++ src/components/FavoriteCard/FavoriteCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './FavoriteCard.css';
 
-const FavoriteCard = () => {
+const FavoriteCard = ({ id, quote, author, picture}) => {
   return (
-    <h1>Here!</h1>
+    <h1>{quote}</h1>
   )
 }
 
--- src/components/FavoritesContainer/FavoritesContainer.js
+++ src/components/FavoritesContainer/FavoritesContainer.js
@@ -1,12 +1,20 @@
 import React, {useState, useEffect} from 'react';
+import FavoriteCard from '../FavoriteCard/FavoriteCard';
 import './FavoritesContainer.css';
 
 const FavoritesContainer = ({ allFavorites }) => {
 
-  const showFavorites = allFavorites.map(favorite => console.log(favorite))
+  const showFavorites = allFavorites.map(favorite => 
+    <FavoriteCard 
+      key={favorite.id}
+      id={favorite.id}
+      quote={favorite.quote}
+      author={favorite.author}
+      picture={favorite.currentPhoto}      
+      />)
 
   return (
-    <h1>{showFavorites}}</h1>
+    <h1>{showFavorites}</h1>
   )
 
 }
