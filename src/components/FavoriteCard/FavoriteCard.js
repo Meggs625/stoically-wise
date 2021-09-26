0aa5e6cf2b7e2a468651fa11b3254c33a0565ddb
Megan McBride <megan.mcbride@colorado.edu>
2021-09-26 09:39:08 -0600

    Start styling the favorites cards

--- src/components/FavoriteCard/FavoriteCard.js
+++ src/components/FavoriteCard/FavoriteCard.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './FavoriteCard.css';
+import deleteIcon from '../../images/delete.png'
 
 const FavoriteCard = ({ id, quote, author, picture}) => {
   return (
-    <section>
-      <img src={picture} alt='selected theme' />
-      <div>
-        <p>{quote}</p>
-        <p>{author}</p>
+    <section className='card'> 
+      <img src={picture} alt='selected theme' className='saved-img'/>
+      <div className='saved-quote-info'>
+        <p className='saved-quote'>{quote}</p>
+        <p className='saved-author'>{author}</p>
+        <img src={deleteIcon} alt='delete button' className='delete-icon'/>
       </div>
     </section>
   )
