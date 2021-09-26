d81f2c8bd1df17e3f3ae17e85aee32775f1ad989
Megan McBride <megan.mcbride@colorado.edu>
2021-09-26 09:14:42 -0600

    Retrieve Favorites from storage in the FavoritesContainer

--- src/components/FavoritesContainer/FavoritesContainer.js
+++ src/components/FavoritesContainer/FavoritesContainer.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './FavoritesContainer.css';
 
 const FavoritesContainer = () => {
+
+  const [favorites, setFavorites] = useState([])
+
+  useEffect(() => {
+    retrieveFavoritesFromStorage()
+  },[])
+
+  const retrieveFavoritesFromStorage = () => {
+    const retreivedFavorites = 
+      Object.keys(localStorage).filter(key => key !== 'chosenTheme')
+        .map(item => JSON.parse(localStorage.getItem(item)))
+    if(retreivedFavorites) {
+      setFavorites(retreivedFavorites)
+    }
+  }
+
   return (
     <h1>Yip yip</h1>
   )
