32969fdb0feb4e692d1385881f0a8d481a8a4fd9
Megan McBride <megan.mcbride@colorado.edu>
2021-09-26 10:18:42 -0600

    Initial card styling complete

--- src/components/FavoriteCard/FavoriteCard.js
+++ src/components/FavoriteCard/FavoriteCard.js
@@ -5,13 +5,14 @@ import deleteIcon from '../../images/delete.png'
 const FavoriteCard = ({ id, quote, author, picture}) => {
   return (
     <section className='card'> 
+        <div className='delete-container'>
+          <button className='delete-btn rotate-scale-up'><img src={deleteIcon} alt='delete button' className='delete-icon'/></button>
+        </div>
       <div className='img-container'>      
         <img src={picture} alt='selected theme' className='saved-img'/>
       </div>
       <div className='saved-quote-info'>
-      <div className='delete-container'>
-        <button className='delete-btn'><img src={deleteIcon} alt='delete button' className='delete-icon'/></button>
-      </div>
+        
         <p className='saved-quote'>{quote}</p>
         <p className='saved-author'>{author}</p>        
       </div>
