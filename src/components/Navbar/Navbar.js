16fc9fc6bc14b5da4cc0c2678985e0cd4795c703
Megan McBride <megan.mcbride@colorado.edu>
2021-09-26 10:40:28 -0600

    Make the brand name a return to home link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,15 +6,17 @@ import owlLogo from '../../images/owl.png'
 const Navbar = () => {
   const location = useLocation();
   return (
-    <section className='nav-bar'>    
-      <div className='brand'>
-        <img src={owlLogo} alt='owl logo' className='logo'/>
-        <h1 className='brand-name'>Daily Wisdom</h1>
-      </div>
+    <section className='nav-bar'>  
+      <Link to='/' className='home-link'>  
+        <div className='brand'>
+          <img src={owlLogo} alt='owl logo' className='logo'/>
+          <h1 className='brand-name'>Daily Wisdom</h1>
+        </div>
+      </Link>
       {location.pathname === '/quote' && 
         <div className='links'>
-          <Link to='/'>Select New Theme</Link>
-          <Link to='/favorites'>View Favorites</Link>      
+          <Link to='/' className='new-theme'>Select New Theme</Link>
+          <Link to='/favorites' className='view-favorites'>View Favorites</Link>      
         </div>}
     </section>
   )
